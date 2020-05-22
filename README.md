94f3c406bbaaefc71bc1a168d7908db5effa9dc5
luotj <luotj@ganguo.hk>
2020-05-22 16:09:01 +0800

    fix:初始化项目运行

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # springboot-jooq
 springboot 集成 jooq 技术操作数据库
+
+
+
+# 该项目访问
+http://localhost:(Your set port)/swagger-ui.html?
+example:    http://localhost:8080/swagger-ui.html?
---
+++ src/main/java/com/luotj/jooqtest/Applicantion.java
@@ -0,0 +1,39 @@
+package com.luotj.jooqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author create by luotj
+ * @Date: 2020/5/22 3:12 下午
+ **/
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableCaching
+@EnableAsync
+@EnableScheduling
+public class Applicantion {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Applicantion.class, args).start();
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(64);
+        executor.setQueueCapacity(65535);
+        executor.setThreadNamePrefix("AsyncExecutor");
+        executor.initialize();
+        return executor;
+    }
+}
