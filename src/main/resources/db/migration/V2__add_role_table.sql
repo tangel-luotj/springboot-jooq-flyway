651878eeed6ccd68db34f9c49f0d3df1986d6df6
luotj <luotj@ganguo.hk>
2020-05-27 17:25:56 +0800

    feat:springBoot整合flyway技术进行数据库版本控制

---
+++ src/main/resources/db/migration/V2__add_role_table.sql
@@ -0,0 +1,9 @@
+-- 用户表
+CREATE TABLE `role`
+(
+    id              BIGINT(20)          NOT NULL COMMENT '主键ID',
+    user_id         BIGINT(20)          NOT NULL DEFAULT 0 COMMENT '用户ID',
+    role_name       VARCHAR(64)         NOT NULL DEFAULT '' COMMENT '角色名称',
+    create_at       BIGINT(50)          NOT NULL DEFAULT 0 COMMENT '创建时间',
+    PRIMARY KEY (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT '全局配置表';
